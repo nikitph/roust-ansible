da117848bc2c32ec637d3edc0010b6b30e561d40
nikit <nikitph@gmail.com>
2016-09-07 20:06:13 -0700

    Update enferno.sh

--- roles/enferno/templates/enferno.sh
+++ roles/enferno/templates/enferno.sh
@@ -10,7 +10,7 @@ GROUP={{user_name}}
 cd /home/{{user_name}}/{{server_hostname}}
 test -d $LOGDIR || mkdir -p $LOGDIR
 source /home/{{user_name}}/{{server_hostname}}/env/bin/activate
-export ENFERNO_ENV='dev'
+export ENFERNO_ENV='prod'
 exec gunicorn  --timeout=300 manage:app -w $NUM_WORKERS \
     --user=$USER --group=$GROUP --log-level=debug \
     --log-file=$LOGFILE 2>>$LOGFILE -k gevent
